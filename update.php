202ed269906e2f2f6cc8bdb86265b5a12ea8b722
dasetiawan <38515985+dasetiawan@users.noreply.github.com>
2018-05-14 00:44:56 +0700

    end

--- update.php
+++ update.php
@@ -1,23 +1,24 @@
 <?php
 include("koneksi.php");
 if(isset($_POST['save'])){
+    $username   = $_POST['username'];
+    $blog       = $_POST['blog'];
+    $email      = $_POST['email'];
+    $phone      = $_POST['phone'];
+    $password   = $_POST['password'];
     session_start();
     if (empty($_SESSION['username'])){
         header("location:loginRegister.php");
     }else{
-        $username   = $_POST['username'];
-        $blog       = $_POST['blog'];
-        $email      = $_POST['email'];
-        $phone      = $_POST['phone'];
-        $password   = $_POST['password'];
-
         $q=mysqli_query($con,"UPDATE user SET username='$username', blog='$blog', email='$email', phone='$phone' where sandi='$password'");
         $r = mysqli_query($con, $q);
-    }
+        session_destroy();   
     if($r){
-        header('location:logout.php');
-    }else{
+        
         header('location:profile.php');
+    }else{
+        header('location:profilEdit.php');
     }
 }   
+}
 ?>
