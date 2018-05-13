c9e38d90857a56f7b1bda30dc5ef0cb28b64dc5b
dasetiawan <38515985+dasetiawan@users.noreply.github.com>
2018-05-13 22:56:44 +0700

    aa

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+
+$username   = $_POST['username'];
+$blog       = $_POST['blog'];
+$email      = $_POST['email'];
+$phone      = $_POST['phone'];
+$password   = $_POST['password'];
+
+$query=mysqli_query($con,"UPDATE user SET username='$username',blog='$blog',phone='$phone', sandi='$password'");
+$r = mysqli_query($con, $query);
+
+if($input){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+     
+    $login = mysqli_query($con,"select * from user where username='$username' and sandi='$password'");
+    $cek = $login->fetch_object();
+    session_start();
+    $_SESSION['username'] = $username;
+    header('location:profile.php');
+ }else{
+  header('location:profilEdit.php');
+ }
+
+?>
